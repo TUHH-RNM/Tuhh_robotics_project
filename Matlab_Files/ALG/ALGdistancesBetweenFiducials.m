ff9aa8e651f0c2dd8fc712f921c02208a1b16b4a
Nasser Attar <nasser.attar@hotmail.com>
2016-06-30 10:41:56 +0200

    Created function ALGdistancesBetweenFiducials to measure the distances between all pairs of a set of fiducials

---
+++ Matlab_Files/ALG/ALGdistancesBetweenFiducials.m
@@ -0,0 +1,12 @@
+function [distances,pairs] = ALGdistancesBetweenFiducials(fiducials)
+% ALGDISTANCEBETWEENFIDUCIALS returns the distance between every pair of fiducials
+%
+%   Author: Nasser Attar
+%   Date created:   30.06.2016
+%   Last modified:  30.06.2016
+%   Change Log:
+
+
+
+% End of function
+end
