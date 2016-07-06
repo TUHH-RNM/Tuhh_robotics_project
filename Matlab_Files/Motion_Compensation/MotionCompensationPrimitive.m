b4113c314dd629b81304f6d4926b2d60dbb62790
rnm_grp3 <rnm_grp3>
2016-07-06 14:24:11 +0200

    Augmented the Motion compensation function and added some new features

--- Matlab_Files/Motion_Compensation/MotionCompensationPrimitive.m
+++ Matlab_Files/Motion_Compensation/MotionCompensationPrimitive.m
@@ -1,4 +1,4 @@
-function MotionCompensationPrimitive(robObj,cameraObj,X,Y,T_C_H_des,cameraFlag,varargin)
+function MotionCompensationPrimitive(robObj,cameraObj,X,Y,T_C_H_des,initialConfig,varargin)
 % MOTIONCOMPENSATIONPRIMITIVE moves the robot such that Tch_des is the HMT from head to coil
 %
 %   
@@ -13,13 +13,16 @@ function MotionCompensationPrimitive(robObj,cameraObj,X,Y,T_C_H_des,cameraFlag,v
 %   Change Log:     
 
 pause('on')
-initialConfig = UR5sendCommand(robObj,'GetStatus');
 
 % Get the HMT from the Head to the Camera. This function is not available
 % at the moment
 % T_TS_H = GetHeadToCameraHMT(cameraObj,cameraFlag);
-T_TS_H = cameraObj.getTransformMatrix();
-Z = Y*T_TS_H*invertHTM(T_C_H_des);
+[T_TS_H,visibility,~] = cameraObj.getTransformMatrix();
+if ~visibility
+    warning('Head is not visible\n')
+    return
+end
+ Z = Y*T_TS_H*invertHTM(T_C_H_des);
 % Get the target HMT from Endeffector to Base to achieve the desired HMT
 % from Head to camera (T_C_H_des)
 T_B_E_des = Z*invertHTM(X);
@@ -30,22 +33,26 @@ row3 = num2str(T_B_E_des(3,:));
 
 % Move robot to the desired pose. This moving should be done with a function 
 % which also features collision detection
-command = ['MoveMinChangeRowWiseStatus ' row1 row2 row3 initialConfig];
-
-if ~strcmp(command,'true')
+%currentHMT_B_E = UR5getPositionHomRowWise(robObj);
+command = ['MoveMinChangeRowWiseStatus ' row1,' ',row2,' ',row3,' ','noToggleHand noToggleElbow noToggleArm'];
+% Give the robot some time for rest
+pause(0.002);
+output = UR5sendCommand(robObj,'BRAKE');
+output = UR5sendCommand(robObj,command);
+if ~strfind(output,'true')
     warning('\nMotion Compensation was not successful\n')
 end
 
 % Wait until the joints don't change anymore (robot arrived)
-temp2 = 0;
-while 1
-    temp1 = UR5getPositionJoints(robObj);
-    if isequal(temp1,temp2)
-        break;
-    end
-    temp2 = temp1;
-    pause(0.5);
-end
+% temp2 = 0;
+% while 1
+%     temp1 = UR5getPositionJoints(robObj);
+%     if isequal(temp1,temp2)
+%         break;
+%     end
+%     temp2 = temp1;
+%     pause(0.5);
+% end
 
 % End of function
 end
--- Matlab_Files/Motion_Compensation/MotionCompensationTest.m
+++ Matlab_Files/Motion_Compensation/MotionCompensationTest.m
@@ -2,32 +2,51 @@
 
 %% Connect with the robot
 robObj = UR5connectRobot('134.28.45.95');
+UR5sendCommand(robObj,'SetSpeed 120');
+UR5sendCommand(robObj,'SetVerbosity 4');
 
 %% Connect with the Tracking Server for Hand-Eye Calibration
-trackObj = GetTrackingObject('coil');
+trackObjCoil = GetTrackingObject('coil');
 
 %% Hand-Eye Calibration
 
 load('DenHartParameters');
-[ randomPose, trackedPose] = HandEyeCalibrationCollectingData(robObj, trackObj, DenHartParameters, 20, 50,'maxRotAngle',30,'maxXYZtrans',200);
+[ randomPose, trackedPose] = HandEyeCalibrationCollectingData(robObj, trackObjCoil, DenHartParameters, 40, 70,'maxRotAngle',30,'maxXYZtrans',200);
 [X, Y] = HandEyeCalibrationCalculatingXY(randomPose, trackedPose);
 
+%% Print the number of valid measurements
+fprintf('Number of valid measurements %d\n',nnz(~isnan(randomPose(1,1,:))));
+
+%% Compute the maximum error 
+errorMax = 0;
+for i=1:70
+    E = (randomPose(:,:,i)*X/trackedPose(:,:,i))/Y;
+    if errorMax < sqrt(E(1,4)^2 + E(2,4)^2 + E(3,4)^2) && ~isnan(E(1,1));
+        errorMax = sqrt(E(1,4)^2 + E(2,4)^2 + E(3,4)^2);
+    end
+end
 %% Connect with the Tracking Server for motion compensation
-trackObj = GetTrackingObject('head');
+trackObjHead = GetTrackingObject('head');
 
 %% Specify the desired transformation from Head to Coil
 % Important !!! Bring the coil in the desired position relative to the coil
-T_B_E = UR5getPositionHomRowWise(robObj);
-T_B_C = T_B_E*X;
 
-T_TS_H = trackObj.getTransformMatrix();
+T_TS_H = trackObjHead.getTransformMatrix();
 T_B_H = Y*T_TS_H;
 
+T_B_E = UR5getPositionHomRowWise(robObj);
+T_B_E(1:3,4) = T_B_E(1:3,4)*1000;
+T_B_C = T_B_E*X;
 T_C_H_des = invertHTM(T_B_C)*T_B_H;
+
 %% Dot the actual Motion Compensation
+% command = 'EnableAlter';
+% % Enable real time mode
+% UR5sendCommand(robObj,command); 
+initialConfig = UR5sendCommand(robObj,'GetStatus');
 h = figure('KeyPressFcn','keep=0');
 keep = true;
 while keep
-    MotionCompensationPrimitive(robObj,trackObj,X,Y,T_C_H_des,'tr')
-    pause(0.1)
+    MotionCompensationPrimitive(robObj,trackObjHead,X,Y,T_C_H_des,initialConfig)
+    pause(0.01)
 end
