e3f58175dc68513a035cb40356d3662033eb3f14
Nasser Attar <nasser.attar@hotmail.com>
2016-07-13 08:55:10 +0200

    Adjusted the Head frame description

--- Matlab_Files/Motion_Compensation/MotionCompensationTest.m
+++ Matlab_Files/Motion_Compensation/MotionCompensationTest.m
@@ -1,5 +1,12 @@
 %% Script for Motion Compensation
-
+% Specify whether Kinect or Atrcsys should be used for tracking
+prompt = 'Shall the Kinect be used?\nIf yes, enter a number above 0\n';
+x = input(prompt);
+if x > 0
+    useKinect = true;
+else
+    useKinect = false;
+end
 %% Connect with the robot
 % Before executing these commands, invoke the (non-blocking) rob6server
 robObj = UR5connectRobot('134.28.45.95');
@@ -25,6 +32,14 @@ for i=1:70
         errorMax = sqrt(E(1,4)^2 + E(2,4)^2 + E(3,4)^2);
     end
 end
+
+%% Compute T_B_K
+% The Y from the Hand-Eye Calibration is the HTM from the Tracking system
+% to base. If the Kinect shall be used we need the HMT T_TS_K
+if useKinect
+    T_TS_K = eye(4);
+    Y = Y*T_TS_K;
+end
 %% Connect with the Tracking Server for head tracking
 trackObjHead = GetTrackingObject('head');
 
@@ -33,17 +48,27 @@ kin = KINECT_initialize('head',999999999999);
 
 %% Specify the desired transformation from Head to Coil
 % Important !!! Bring the coil in the desired position relative to the coil
-[T_TS_H,visibility,~] = trackObjHead.getTransformMatrix();
-% [T_TS_H,visibility] = KINECT_getMarkerFrameHMT(kin);
-if ~visibility
-    warning('Head is not visible\n')
-end
-T_B_H = Y*T_TS_H;
+% if useKinect
+%     [T_TS_H,visibility] = KINECT_getMarkerFrameHMT(kin,'headFrame');
+% else
+%     [T_TS_H,visibility,~] = trackObjHead.getTransformMatrix();
+% end
+% if ~visibility
+%     warning('Head is not visible\n')
+% end
+% T_B_H = Y*T_TS_H;
+% 
+% T_B_E = UR5getPositionHomRowWise(robObj);
+% T_B_E(1:3,4) = T_B_E(1:3,4)*1000;
+% T_B_C = T_B_E*X;
+% T_C_H_des = invertHTM(T_B_C)*T_B_H;
 
-T_B_E = UR5getPositionHomRowWise(robObj);
-T_B_E(1:3,4) = T_B_E(1:3,4)*1000;
-T_B_C = T_B_E*X;
-T_C_H_des = invertHTM(T_B_C)*T_B_H;
+% Demanded by the project task
+T_H_C_des = [0.868967 0.203085 -0.451281 48.631371;
+             0.331437 -0.916017 0.225975 -85.534794;
+             -0.367487 -0.345935 -0.863298 126.256582;
+             0 0 0 1];
+T_C_H_des = invertHTM(T_H_C_des);         
 
 %% Do the actual Motion Compensation
 % command = 'EnableAlter';
