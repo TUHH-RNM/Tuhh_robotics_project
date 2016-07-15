22f8c6a65dd6ac45a25fc4bce0f7e50528f146a0
rnm_grp3 <rnm_grp3>
2016-07-15 14:17:55 +0200

    Final change

--- Matlab_Files/ALG/ALGcreateCoordinates.m
+++ Matlab_Files/ALG/ALGcreateCoordinates.m
@@ -30,10 +30,10 @@ if((numberDimm > 3) || (numberDimm < 2))
 end
 
 %% Calculation
-    diffVec21 = points(2,:) - points(1,:);
+    diffVec21 = points(3,:) - points(1,:);
     Cx = diffVec21/ALGlenghtVec(diffVec21);
     
-    diffVec31 = points(3,:) - points(1,:);
+    diffVec31 = points(2,:) - points(1,:);
     proj = dot(Cx,diffVec31)*Cx;
     diffVec31 = diffVec31 - proj;
     Cy        = diffVec31/ALGlenghtVec(diffVec31);
